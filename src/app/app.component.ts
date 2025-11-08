12236ce96b2099c6ccfa613db3829c5f2f76b2bb
Levin Bänninger <l.baenninger@icloud.com>
2025-11-08 14:05:19 +0100

    chore: set up tailwind

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  imports: [RouterModule],
+  selector: 'app-root',
+  template: `<h1 class="text-3xl font-bold underline">Hello World</h1>`,
+})
+export class AppComponent {}
